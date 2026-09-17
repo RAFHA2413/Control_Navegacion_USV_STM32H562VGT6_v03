548c4eda3d18a878e07db439f57f91a18aadc485
RAFHA2413 <rafhael.lopez.ca@gmail.com>
2026-09-17 17:42:25 -0500

    Ampliar prueba MG996R a pulsos 500-2500 us

--- Core/LIBRERIAS/servos.c
+++ Core/LIBRERIAS/servos.c
@@ -5,113 +5,93 @@
  *      Author: Alcides Ramos
  */
 
-
 #include "servos.h"
 
-
-//incluir los timer usados
+/* incluir los timer usados */
 extern TIM_HandleTypeDef htim3;
 
-
 /*
- * PRUEBA TEMPORAL CONTINUA DEL SERVO DE CAMARA
+ * PRUEBA TEMPORAL CONTINUA DEL SERVO DE CAMARA MG996R
  *
- * Al iniciar el STM32, el servo se centra y luego conmuta
- * indefinidamente entre -90 y +90 grados.
+ * El servo probado recorrio aproximadamente +/-45 grados con
+ * pulsos de 1000 a 2000 us. Para validar el recorrido completo
+ * del MG996R se prueba ahora el rango ampliado 500 a 2500 us.
  *
  * Con la configuracion actual de TIM3 (1 us por cuenta):
- *   -90 grados = 1000 us
- *     0 grados = 1500 us
- *   +90 grados = 2000 us
+ *    extremo 1 =  500 us
+ *    centro    = 1500 us
+ *    extremo 2 = 2500 us
  *
- * Esta prueba es temporal. Al finalizar la validacion del servo,
- * se debe volver a SERVO_init() normal para permitir que el resto
- * del firmware continue su inicializacion.
+ * La prueba es intencionalmente infinita y bloquea el resto del
+ * firmware mientras se valida exclusivamente el servo.
  */
 #define SERVO_PRUEBA_RETARDO_MS  1500U
+#define SERVO_PULSO_MIN_US       500.0f
+#define SERVO_PULSO_CENTRO_US    1500.0f
+#define SERVO_PULSO_MAX_US       2500.0f
 
-
-//Configurar Servos Usados
-SERVOS SERVO1 = {&htim3, &(TIM3->CCR3),TIM_CHANNEL_3};
-
-
-
-
+/* Configurar Servos Usados */
+SERVOS SERVO1 = {&htim3, &(TIM3->CCR3), TIM_CHANNEL_3};
 
 void SERVO_init(SERVOS *servo)
 {
     HAL_TIM_PWM_Start(servo->htim, servo->channel);
 
-    /*
-     * Primero lleva el servo al centro.
-     */
-    SERVO_ANG(servo, 0.0f);
+    /* Centro antes de iniciar la prueba de extremos. */
+    SERVO_MICRO(servo, SERVO_PULSO_CENTRO_US);
     HAL_Delay(SERVO_PRUEBA_RETARDO_MS);
 
     /*
-     * PRUEBA INFINITA:
-     * -90 grados -> +90 grados -> -90 grados -> ...
-     *
-     * El salto completo entre ambos extremos permite verificar
-     * el recorrido total del servo con pulsos de 1000 a 2000 us.
+     * PRUEBA INFINITA DEL RECORRIDO COMPLETO:
+     * 500 us -> 2500 us -> 500 us -> 2500 us -> ...
      */
     while (1)
     {
-        SERVO_ANG(servo, -90.0f);
+        SERVO_MICRO(servo, SERVO_PULSO_MIN_US);
         HAL_Delay(SERVO_PRUEBA_RETARDO_MS);
 
-        SERVO_ANG(servo, 90.0f);
+        SERVO_MICRO(servo, SERVO_PULSO_MAX_US);
         HAL_Delay(SERVO_PRUEBA_RETARDO_MS);
     }
 }
 
-
-void SERVO_ANG(SERVOS *servo,float posi)
-
+void SERVO_ANG(SERVOS *servo, float posi)
 {
-     float calcu;
-      calcu=(ser_lim_sup_ms - ser_lim_inf_ms) / (ser_sup - ser_inf);
-      calcu=calcu*(posi-ser_inf);
-      calcu=(calcu+ser_lim_inf_ms)*1000.0;// para pasarlo a microsegundo
-      *servo->ccr = calcu;
-
+    float calcu;
+    calcu = (ser_lim_sup_ms - ser_lim_inf_ms) / (ser_sup - ser_inf);
+    calcu = calcu * (posi - ser_inf);
+    calcu = (calcu + ser_lim_inf_ms) * 1000.0f;
+    *servo->ccr = (uint32_t)calcu;
 }
 
-
-void SERVO_MICRO(SERVOS *servo,float micro)
-
+void SERVO_MICRO(SERVOS *servo, float micro)
 {
-     *servo->ccr = micro;
+    *servo->ccr = (uint32_t)micro;
 }
 
-
-void SERVO_MILI(SERVOS *servo,float milis)
-
+void SERVO_MILI(SERVOS *servo, float milis)
 {
-     float calcu;
-      calcu=milis*1000.0;// para pasarlo a microsegundo
-      *servo->ccr = calcu;
+    float calcu;
+    calcu = milis * 1000.0f;
+    *servo->ccr = (uint32_t)calcu;
 }
 
-
-void SERVO_MUEVE(SERVOS *servo,float ini, float final,float paso,float ret)
+void SERVO_MUEVE(SERVOS *servo, float ini, float final, float paso, float ret)
 {
-if (final>ini)
-{               //-90    90
-  for   (float c=ini; c<final;c+=paso)
-  {
-    SERVO_ANG(servo, c);
-    HAL_Delay(ret) ;
-  }
-
-}
-else
-{                  //90     -90
-      for   (float c=ini; c>final;c-=paso)
-      {
-        SERVO_ANG(servo, c);
-        HAL_Delay(ret) ;
-      }
-
-}
+    if (final > ini)
+    {
+        for (float c = ini; c < final; c += paso)
+        {
+            SERVO_ANG(servo, c);
+            HAL_Delay((uint32_t)ret);
+        }
+    }
+    else
+    {
+        for (float c = ini; c > final; c -= paso)
+        {
+            SERVO_ANG(servo, c);
+            HAL_Delay((uint32_t)ret);
+        }
+    }
 }
