0aeb00abe53efe473d7b8ee20dfab31a77d8e41e
RAFHA2413 <rafhael.lopez.ca@gmail.com>
2026-09-21 15:31:16 -0500

    5.12 PRUEBA VIDA PB2 CON IRQ GPDMA1 CH2 HABILITADA

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -764,8 +764,8 @@ static void MX_GPDMA1_Init(void)
   /* GPDMA1 interrupt Init */
     HAL_NVIC_SetPriority(GPDMA1_Channel0_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(GPDMA1_Channel0_IRQn);
-    //HAL_NVIC_SetPriority(GPDMA1_Channel2_IRQn, 0, 0);
-    //HAL_NVIC_EnableIRQ(GPDMA1_Channel2_IRQn);
+    HAL_NVIC_SetPriority(GPDMA1_Channel2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(GPDMA1_Channel2_IRQn);
 
   /* USER CODE BEGIN GPDMA1_Init 1 */
 
